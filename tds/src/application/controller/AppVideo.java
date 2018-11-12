e6c2858edf267720da13d11fa83d5ff31b2164c8
hidsor <vladgre98@gmail.com>
2018-11-12 19:37:11 +0100

    Avances con la interfaz gráfica de varios días

--- tds/src/application/controller/AppVideo.java
+++ tds/src/application/controller/AppVideo.java
@@ -1,10 +1,66 @@
 package application.controller;
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
 
 public class AppVideo {
 
-	// Clase controlador de APPVideo (OurTube)
-	public AppVideo() {
-		// TODO Apéndice de constructor generado automáticamente
-	}
+	private double oldheight = 0;
+	private double oldwidth = 0;
+	
+    @FXML
+    private JFXButton minimize;
 
+    @FXML
+    private JFXButton maximize;
+
+    @FXML
+    private JFXButton mislistas;
+
+    @FXML
+    private JFXButton premium;
+
+    @FXML
+    private JFXButton explorar;
+
+    @FXML
+    private JFXButton recientes;
+
+    @FXML
+    private JFXButton login;
+
+    @FXML
+    private JFXButton close;
+
+    @FXML
+    private JFXButton nuevalista;
+
+
+    @FXML
+    void minimizeWindow(ActionEvent event) {
+    	((Stage)((JFXButton)event.getSource()).getScene().getWindow()).setIconified(true);
+    	
+    }
+
+    @FXML
+    void maximizeWindow(ActionEvent event) {
+    	Stage stg = ((Stage)((JFXButton)event.getSource()).getScene().getWindow());
+    	
+    	if (stg.isMaximized()) {
+    		stg.setWidth(oldwidth);
+    		stg.setHeight(oldheight);
+    		stg.setMaximized(false);
+    	} else {
+    		oldwidth = stg.getWidth();
+    		oldheight = stg.getHeight();
+    		stg.setMaximized(true);
+    	}
+    }
+
+    @FXML
+    void closeWindow(ActionEvent event) {
+    	((Stage)((JFXButton)event.getSource()).getScene().getWindow()).close();
+    }
 }
--- tds/src/application/view/Main.java
+++ tds/src/application/view/Main.java
@@ -1,19 +1,43 @@
 package application.view;
 	
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class Main extends Application {
+	
+	private double xOffset = 0; 
+	private double yOffset = 0;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("Root.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+		      root.setOnMousePressed(new EventHandler<MouseEvent>() {
+		            public void handle(MouseEvent event) {
+		                xOffset = event.getSceneX();
+		                yOffset = event.getSceneY();
+		            }
+		        });
+		        
+		        //move around here
+		        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+		            public void handle(MouseEvent event) {
+		                primaryStage.setX(event.getScreenX() - xOffset);
+		                primaryStage.setY(event.getScreenY() - yOffset);
+		            }
+		        });
+
+			Scene scene = new Scene(root,800,600);
+			scene.getStylesheets().add(getClass().getResource("applicationred.css").toExternalForm());
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -21,6 +45,7 @@ public class Main extends Application {
 		}
 	}
 	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
